77f9aaa5a90f96f2ab440f23fb2a682ba6ed5b10
Fairoz007 <fairozfaisal2001@gmail.com>
2024-10-10 17:53:39 +0530

    time sheet

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // The password is stored as a hashed value
-});
-
-module.exports = mongoose.model('User', userSchema);
